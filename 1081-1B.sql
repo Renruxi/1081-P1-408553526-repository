a56686b36840d4449995cccdcdc8245de90cb5d0
Renruxi <786724558@qq.com>
2019-12-28 15:32:58 +0800

    write your log message

---
+++ 1081-1B.sql
@@ -0,0 +1,15 @@
+/*
+-- Query: SELECT * FROM demo
+LIMIT 0, 1000
+
+-- Date: 2019-12-28 14:46
+*/
+INSERT INTO `` (`id`,`name`,`pic`,`href`) VALUES (1,'Intro','intro.png','/tku/w01/intro.html');
+INSERT INTO `` (`id`,`name`,`pic`,`href`) VALUES (2,'Pokemon','pokemon.png','/tku/w01/pokemon.html');
+INSERT INTO `` (`id`,`name`,`pic`,`href`) VALUES (3,'Gallery','gallery.png','/tku/w02/imageGallery.html');
+INSERT INTO `` (`id`,`name`,`pic`,`href`) VALUES (4,'Blog','blog.png','/tku/w02/blog.html');
+INSERT INTO `` (`id`,`name`,`pic`,`href`) VALUES (5,'Div','div.png','/tku/w03/div.html');
+INSERT INTO `` (`id`,`name`,`pic`,`href`) VALUES (6,'TKU','tku60.png','/tku/w04/tku60.html');
+INSERT INTO `` (`id`,`name`,`pic`,`href`) VALUES (7,'My Class ','myclass.png','/tku/My class/myclass.html');
+INSERT INTO `` (`id`,`name`,`pic`,`href`) VALUES (8,'My Resume','myresume.png','/tku/My resume/resume.html');
+INSERT INTO `` (`id`,`name`,`pic`,`href`) VALUES (9,'Calculator','cal.png','/tku/w09_calculator/calc_id.html');
